098db251216529fd29e7edb9efe2bfd4be97ab9b
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2025-10-23 21:46:15 +0200

    Change highest number to 10 in main method GuessingGame

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,8 +1,15 @@
 public class GuessingGame {
     public static void main(String[] args) {
         
-        Guesser guess = new Guesser(0, 1000); 
+        Guesser guess = new Guesser(0, 10); 
 
-        guess.start();
+        
+
+            guess.start();
+
+
+        
+
+
     }
 }
